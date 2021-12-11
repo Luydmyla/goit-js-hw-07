fa29a8928867f18146789c098e7043abf53904fd
Luydmyla <lytic.ok@gmail.com>
2021-12-11 16:07:41 +0200

    add task - 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,32 +4,27 @@ import { galleryItems } from './gallery-items.js';
 console.log(galleryItems);
 const refs = {
     list: document.querySelector('.gallery'),
-
 };
 console.log(refs.list);
 
-// const listImageElenents = createListImageMarcup(galleryItems);
-// console.log(listImageMarcup);
-const createListImageElements = galleryItems.map(galleryItem => `<li style = "list-style-type: none"> <div class="gallery__item"> <a class="gallery__link" href="${galleryItem.original}"> <img class="gallery__image" src = "${galleryItem.preview}" data-source="${galleryItem.original}" alt = "${galleryItem.description}" />   </a>  </div>  </li>`).join("");
-  
-  console.log(createListImageElements);
-refs.list.innerHTML = createListImageElements;
-// refs.list.insertAdjacentHTML("beforeend", createListImageElements)
-console.log(refs.list);
- 
-refs.list.addEventListener('click', onGalleryItemsClick)
+const listImageElements = createListImageElements(galleryItems);
+function createListImageElements(galleryItems) {
+    return galleryItems.map(galleryItem => `<li style = "list-style-type: none"> <div class="gallery__item"> <a class="gallery__link" href="${galleryItem.original}"> <img class="gallery__image" src = "${galleryItem.preview}" data-source="${galleryItem.original}" alt = "${galleryItem.description}" /> </a> </div> </li>`).join("");
+}
+// console.log(listImageElements);
+// refs.list.innerHTML = listImageElements;
+refs.list.insertAdjacentHTML("beforeend", listImageElements)
+
+refs.list.addEventListener('click', onGalleryItemsClick)  
 
 function onGalleryItemsClick(event) {
-      console.log(event.target);
+    if (event.target.nodeName != 'IMG') {
+    return
+  } else {
+    event.preventDefault();
+     //  console.log(event.target.nodeName);
+    // console.log(event.target);
+    // console.log(event.target.dataset.source);
+    refs.list.onclick = () => { basicLightbox.create(`<img width="1400" height="900" src= "${event.target.dataset.source}">`).show() }
   }
-//  import * as basicLightbox from 'basiclightbox' 
-const instance = basicLightbox.create(`
-    <div class="modal">
-        <p>
-            Your first lightbox with just a few lines of code.
-            Yes, it's really that simple.
-        </p>
-    </div>
-`)
-
-instance.show()
+  }
