af9e9fe1ec7d1e99bd9d843711b4c1f1bc02bb58
Luydmyla <lytic.ok@gmail.com>
2021-12-12 01:04:10 +0200

    fixed task - 1
    -- gallery 01 :
      fixed  callback -f : delete onclick
    -- add coments;

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,30 +1,36 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
-console.log(galleryItems);
+// --- ----нашла ссылку на элемент div.gallery ------------
+// console.log(galleryItems);
 const refs = {
     list: document.querySelector('.gallery'),
 };
 console.log(refs.list);
-
+// --------------создаю разметку по шаблону (шаблонная строка)----------
 const listImageElements = createListImageElements(galleryItems);
 function createListImageElements(galleryItems) {
     return galleryItems.map(galleryItem => `<li style = "list-style-type: none"> <div class="gallery__item"> <a class="gallery__link" href="${galleryItem.original}"> <img class="gallery__image" src = "${galleryItem.preview}" data-source="${galleryItem.original}" alt = "${galleryItem.description}" /> </a> </div> </li>`).join("");
 }
 // console.log(listImageElements);
+
+// --------------рендер разметки (2 способа)-------------
 // refs.list.innerHTML = listImageElements;
 refs.list.insertAdjacentHTML("beforeend", listImageElements)
 
-refs.list.addEventListener('click', onGalleryItemsClick)  
-
+// -------------------вешаю слушатель событий на див с создаными элементами-----------
+refs.list.addEventListener('click', onGalleryItemsClick)
+ 
+// ------------колбек - функция - обработчик событый -------- 
 function onGalleryItemsClick(event) {
-    if (event.target.nodeName != 'IMG') {
+  // -----------запрет для браузера на действия по умолчанию (чтоб не открывал картинку по ссылке )------------
+   event.preventDefault();
+    // console.log(event.target);
+    // console.log(event.target.nodeName);
+  // --------проверяю куда попадает мой клик - если не в картинку, то выходим и ничего не делаем)-------------
+  if (event.target.nodeName != 'IMG') {
     return
   } else {
-    event.preventDefault();
-     //  console.log(event.target.nodeName);
-    // console.log(event.target);
+    // -------иначе, используя библиотеку бейсиклайтбокс, открываем оригинальное изображение в модальном окне-----------
     // console.log(event.target.dataset.source);
-    refs.list.onclick = () => { basicLightbox.create(`<img width="1400" height="900" src= "${event.target.dataset.source}">`).show() }
-  }
+  basicLightbox.create(`<img width="1400" height="900" src= "${event.target.dataset.source}">`).show() }
   }
