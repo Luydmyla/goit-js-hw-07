74da6d846a7882a0f7549e52d525e410a7a27ea3
Luydmyla <lytic.ok@gmail.com>
2021-12-13 02:10:41 +0200

    add task-2
    -fixed task -02
    -add extra-task in task-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -9,7 +9,8 @@ console.log(refs.list);
 // --------------создаю разметку по шаблону (шаблонная строка)----------
 const listImageElements = createListImageElements(galleryItems);
 function createListImageElements(galleryItems) {
-    return galleryItems.map(galleryItem => `<li style = "list-style-type: none"> <div class="gallery__item"> <a class="gallery__link" href="${galleryItem.original}"> <img class="gallery__image" src = "${galleryItem.preview}" data-source="${galleryItem.original}" alt = "${galleryItem.description}" /> </a> </div> </li>`).join("");
+  return galleryItems.map(galleryItem => `<div class="gallery__item"> <a class="gallery__link" href="${galleryItem.original}"> <img class="gallery__image" src = "${galleryItem.preview}" data-source="${galleryItem.original}" alt = "${galleryItem.description}" /> </a> </div>`).join("");
+  //  return galleryItems.map(galleryItem => `<li style = "list-style-type: none"> <div class="gallery__item"> <a class="gallery__link" href="${galleryItem.original}"> <img class="gallery__image" src = "${galleryItem.preview}" data-source="${galleryItem.original}" alt = "${galleryItem.description}" /> </a> </div> </li>`).join("");
 }
 // console.log(listImageElements);
 
@@ -23,14 +24,47 @@ refs.list.addEventListener('click', onGalleryItemsClick)
 // ------------колбек - функция - обработчик событый -------- 
 function onGalleryItemsClick(event) {
   // -----------запрет для браузера на действия по умолчанию (чтоб не открывал картинку по ссылке )------------
-   event.preventDefault();
-    // console.log(event.target);
-    // console.log(event.target.nodeName);
+  event.preventDefault();
+  // console.log(event.target);
+  // console.log(event.target.nodeName);
   // --------проверяю куда попадает мой клик - если не в картинку, то выходим и ничего не делаем)-------------
   if (event.target.nodeName != 'IMG') {
     return
-  } else {
-    // -------иначе, используя библиотеку бейсиклайтбокс, открываем оригинальное изображение в модальном окне-----------
-    // console.log(event.target.dataset.source);
-  const instance = basicLightbox.create(`<img width="1400" height="900" src= "${event.target.dataset.source}">`).show() }
+  } // -------иначе, используя библиотеку бейсиклайтбокс, открываем оригинальное изображение в модальном окне-----------
+  else {
+    const instance = basicLightbox.create(`<img width="1400" height="900" src= "${event.target.dataset.source}">`, {
+
+      // ---добавляю слушательсобытий клавиатуры на кнопку эскейп при открытом модальном окне-----------
+      onShow: (instance) => {
+        document.addEventListener("keydown", this.onInstanceclick)
+        // ---колбек-функция -обработчик события ---
+        this.onInstanceclick = function (e) {
+          // console.log(e.target);
+          // ----------закрыла окно по кнопке Эскейп-------------
+          if (e.code === "Escape") { instance.close() }
+       }
+        
+      },
+      // -------снимаю слушателя ссобытий после закрытия модального окна------
+      onClose: (instance) => {
+      document.removeEventListener("keydown", this.onInstanceclick)
+    }
+    }).show()
+    
+
+  //  ---------------ВСЕ---------------
+    
+    //   onShow: (instance) => {
+    //     document.addEventListener("keydown", onclick)
+    
+    //    
+    //     function onclick(e) {
+          // if (e.code === "Escape")
+    //       
+    //       //  e.target =  instance.close
+    //       document.removeEventListener("keydown", onclick);
+    // };
+   
+    //     }
   }
+}
